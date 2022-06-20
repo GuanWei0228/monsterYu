ad80e081bf6899f3adb07e687309fb45b26a8c08
GunaWei0228 <zx910228@gmail.com>
2022-06-20 18:22:26 +0800

    v.3(無聲音)

--- app/src/main/java/tw/edu/pu/s1092790/monsteryu/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1092790/monsteryu/MainActivity.kt
@@ -1,19 +1,25 @@
 package tw.edu.pu.s1092790.monsteryu
 
 import android.graphics.Canvas
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
+import android.view.WindowManager
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import tw.edu.pu.s1092790.monsteryu.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,GestureDetector.OnGestureListener,View.OnTouchListener{
     val db = FirebaseFirestore.getInstance()
     lateinit var binding: ActivityMainBinding
     lateinit var job: Job
+    lateinit var mper: MediaPlayer
+    lateinit var gDetector: GestureDetector
     var num = 87
     var check:Boolean = false
     var attack:Boolean = false
@@ -26,34 +32,15 @@ class MainActivity : AppCompatActivity() {
     var hdef: String = ""
     var startt = 87
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        lateinit var job: Job
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mper = MediaPlayer()
 
-
-
-
-        /*db.collection("monsteryu")
-            .get()
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    var msg: String = ""
-                    for (document in task.result!!) {
-                        msg +=document.data["怪獸防禦力"].toString()
-                    }
-                    if (msg != "") {
-                        txv2.text = msg
-                    } else {
-                        txv2.text = "查無資料"
-                    }
-                }
-            }*/
-
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         binding.img1.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
@@ -61,32 +48,35 @@ class MainActivity : AppCompatActivity() {
                 if (check == false) {
                     img1.setImageResource(R.drawable.start)
                     startt = 87
+                }
+                else {
+                    img1.setImageResource(R.drawable.stop)
+                    startt = 1
                     db.collection("monsteryu")
                         .get()
                         .addOnCompleteListener { task ->
                             if (task.isSuccessful) {
                                 for (document in task.result!!) {
-                                    mhealth += document.data["怪獸血量"].toString()
-                                    matk += document.data["怪獸攻擊力"].toString()
-                                    mdef = document.data["怪獸防禦力"].toString()
-                                    hhealth += document.data["主角血量"].toString()
-                                    hatk = document.data["主角攻擊力"].toString()
-                                    hdef += document.data["主角防禦力"].toString()
+                                    mhealth += document.data["怪獸血量"].toString().toInt()
+                                    matk += document.data["怪獸攻擊力"].toString().toInt()
+                                    mdef += document.data["怪獸防禦力"].toString().toInt()
+                                    hhealth += document.data["主角血量"].toString().toInt()
+                                    hatk += document.data["主角攻擊力"].toString().toInt()
+                                    hdef += document.data["主角防禦力"].toString().toInt()
 
                                 }
-                                txv2.text = hdef
+
+
                             }
                         }
-                }
-                else {
-                    img1.setImageResource(R.drawable.stop)
-                    startt = 1
                 }
             }
         })
 
     job = GlobalScope.launch(Dispatchers.Main) {
             while (true) {
+
+                mper.reset()
                 if(startt == 1)
                 {
                     sec.text = "3"
@@ -100,13 +90,27 @@ class MainActivity : AppCompatActivity() {
                     sec.text = ""
                     matkk.text = "癐務攻擊力:"
                     mheal.text = "癐務血量:"
-                    mdeff.text = "癐務防禦:"
+                    mdeff.text = "癐務防禦力:"
                     hatkk.text = "主角攻擊力:"
                     hheal.text = "主角血量:"
                     hdeff.text = "主角防禦力:"
+
+                    txv2.text = mhealth
+                    txv3.text = matk
+                    txv4.text = mdef
+                    txv5.text = hhealth
+                    txv6.text = hatk
+                    txv7.text = hdef
+
                     startt = 0
                 }
                 if(startt == 87) {
+                    txv2.text = ""
+                    txv3.text = ""
+                    txv4.text = ""
+                    txv5.text = ""
+                    txv6.text = ""
+                    txv7.text = ""
                     matkk.text = ""
                     mheal.text = ""
                     mdeff.text = ""
@@ -114,6 +118,7 @@ class MainActivity : AppCompatActivity() {
                     hheal.text = ""
                     hdeff.text = ""
                 }
+
                 binding.btn.setOnClickListener(object : View.OnClickListener {
                     override fun onClick(p0: View?) {
                         attack = !attack  //check是布林值，要先在前面設var check: Boolean = false
@@ -128,7 +133,7 @@ class MainActivity : AppCompatActivity() {
                         defence = !defence //check是布林值，要先在前面設var check: Boolean = false
                         if(defence == true)
                         {
-                            num = (Math.random()*2).toInt()
+                            num = (Math.random()*5).toInt()
                         }
                     }
                 })
@@ -138,30 +143,128 @@ class MainActivity : AppCompatActivity() {
                     binding.mysv.drawSomething(canvas)
                     binding.mysv.holder.unlockCanvasAndPost(canvas)
                     img2.setImageResource(R.drawable.yu2)
-                    attack = false
+                    mhealth = (Integer.parseInt(mhealth)-(Integer.parseInt(hatk)-Integer.parseInt(mdef))).toString()
+                    txv2.text = Integer.parseInt(mhealth).toString()
+
+                    delay(500)
+                    if(Integer.parseInt(mhealth) == 0) {
+                        img2.setImageResource(R.drawable.yu4)
+                        check = false
+                        img1.setImageResource(R.drawable.start)
+                        attack = false
+                    }
+                    else{
+                        img2.setImageResource(R.drawable.yu1)
+                        attack = false
+                    }
                 }
                 if (check && attack && num == 0) {
                     val canvas: Canvas = binding.mysv.holder.lockCanvas()
                     binding.mysv.drawSomething(canvas)
                     binding.mysv.holder.unlockCanvasAndPost(canvas)
                     img2.setImageResource(R.drawable.yu2)
+                    mhealth = (Integer.parseInt(mhealth)-(Integer.parseInt(hatk)-Integer.parseInt(mdef))).toString()
+                    txv2.text = Integer.parseInt(mhealth).toString()
+
                     delay(500)
-                    img2.setImageResource(R.drawable.yu3)
-                    attack = false
+
+                    if(Integer.parseInt(mhealth) == 0) {
+                        img2.setImageResource(R.drawable.yu4)
+                        check = false
+                        img1.setImageResource(R.drawable.start)
+                        attack = false
+                    }
+                    else{
+                        img2.setImageResource(R.drawable.yu3)
+                        hhealth = (Integer.parseInt(hhealth)-(Integer.parseInt(matk)-Integer.parseInt(hdef))).toString()
+                        txv5.text = Integer.parseInt(hhealth).toString()
+                        delay(500)
+                        img2.setImageResource(R.drawable.yu1)
+                        attack = false
+                    }
                 }
-                if (check && defence && num == 1) {
+
+                if (check && defence && (num == 0 || num == 1 || num == 2 || num == 3)) {
                     val canvas: Canvas = binding.mysv.holder.lockCanvas()
                     binding.mysv.drawSomething(canvas)
                     binding.mysv.holder.unlockCanvasAndPost(canvas)
                     img2.setImageResource(R.drawable.yu3)
-                    defence = false
+                    hhealth = (Integer.parseInt(hhealth)-(Integer.parseInt(matk)-Integer.parseInt(hdef))).toString()
+                    txv5.text = Integer.parseInt(hhealth).toString()
+                    delay(500)
+                    if(Integer.parseInt(hhealth) != 0) {
+                        img2.setImageResource(R.drawable.yu1)
+                        defence = false
+                    }
+                    else{
+                        img2.setImageResource(R.drawable.yu1)
+                        defence = false
+                        check = false
+                        img1.setImageResource(R.drawable.start)
+                    }
+
                 }
+                if (check && defence && num == 4) {
+                    val canvas: Canvas = binding.mysv.holder.lockCanvas()
+                    binding.mysv.drawSomething(canvas)
+                    binding.mysv.holder.unlockCanvasAndPost(canvas)
+                    img2.setImageResource(R.drawable.yu3)
+
+                    if(Integer.parseInt(hhealth) != 0) {
+                        if(Integer.parseInt(matk) != Integer.parseInt(hdef)){
+                            hdef = ((Integer.parseInt(hdef)+1)).toString()
+                            txv7.text = Integer.parseInt(hdef).toString()
+                            defscs.text = "格檔成功，防禦力+1"
+                            delay(500)
+                            defscs.text = ""
+                        }
+                        img2.setImageResource(R.drawable.yu1)
+                        defence = false
+                    }
+                    else{
+                        delay(500)
+                        img2.setImageResource(R.drawable.yu1)
+                        defence = false
+                        check = false
+                        img1.setImageResource(R.drawable.start)
+                    }
+                }
+
                 delay(500)
-                img2.setImageResource(R.drawable.yu1)
+
+
+
 
             }
 
         }
 
+    }
+
+    override fun onDown(p0: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onShowPress(p0: MotionEvent?) {
+    }
+
+    override fun onSingleTapUp(p0: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onScroll(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        return true
+    }
+
+    override fun onLongPress(p0: MotionEvent?) {
+        //TODO("Not yet implemented")
+    }
+
+    override fun onFling(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        return true
+    }
+
+    override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+        return true
     }
 }
